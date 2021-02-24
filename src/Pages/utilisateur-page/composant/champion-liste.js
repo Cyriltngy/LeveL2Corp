bff66523908bb8dd5d1195182d5ff54648f22ba0
Cyriltngy <cyriltanguy44800@gmail.com>
2021-02-24 18:26:28 +0100

    push 24/02

--- src/Pages/utilisateur-page/composant/champion-liste.js
+++ src/Pages/utilisateur-page/composant/champion-liste.js
@@ -1,39 +1,44 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./champion-liste.css";
 import axios from "axios";
 
 function ChampList() {
-  let loadingChamp = true;
-  let tableauChampion = [];
   const [stateImageChamp, setStateImageChamp] = useState([]);
-  tableauChampion = stateImageChamp;
-  const [stateArrayFindChamp, setStateArrayFindChamp] = useState([]);
+  const [stateStringChamp, setStateStringChamp] = useState([]);
+
   function handlerChangeNameChamp(event) {
     if (!isImageChampLoading()) {
       return;
     }
     const saisieNameChamp = event.target.value.toLowerCase();
-    const champFilter = stateImageChamp.filter((filterName) => filterName.toLowerCase().includes(saisieNameChamp));
-    setStateArrayFindChamp(champFilter);
+    setStateStringChamp(saisieNameChamp);
     console.log("CTA", champFilter);
   }
+
   function isImageChampLoading() {
     return stateImageChamp.length > 0;
   }
 
-  if (!isImageChampLoading()) {
-    axios.get(`http://ddragon.leagueoflegends.com/cdn/11.4.1/data/en_US/champion.json`).then((res) => {
-      console.log("CTA before return axios", Object.keys(res.data.data));
-      setStateImageChamp(Object.keys(res.data.data));
-    });
-  }
-  console.log("CTA before return", stateArrayFindChamp);
+  useEffect(() => {
+    if (!isImageChampLoading()) {
+      axios.get(`http://ddragon.leagueoflegends.com/cdn/11.4.1/data/en_US/champion.json`).then((res) => {
+        console.log("CTA before return axios", Object.keys(res.data.data));
+        setStateImageChamp(Object.keys(res.data.data));
+      });
+    }
+  });
+  console.log("CTA before return", stateStringChamp);
+  const champFilter = stateImageChamp.filter((filterName) => filterName.toLowerCase().includes(stateStringChamp));
   return (
-    <div className="championListe">
-      <input type="text" placeholder="Entrez un nom de champion" onChange={handlerChangeNameChamp}></input>
-      {stateArrayFindChamp.map((champion) => (
-        <img key={champion} src={`http://ddragon.leagueoflegends.com/cdn/11.4.1/img/champion/${champion}.png`} />
-      ))}
+    <div className="componentChampionList">
+      <div className="zoneTexte">
+        <input type="text" placeholder="Entrez un nom de champion" onChange={handlerChangeNameChamp}></input>
+      </div>
+      <div className="listeChampion">
+        {champFilter.map((champion) => (
+          <img key={champion} src={`http://ddragon.leagueoflegends.com/cdn/11.4.1/img/champion/${champion}.png`} />
+        ))}
+      </div>
     </div>
   );
 }
